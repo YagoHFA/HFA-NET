b25e5a102ac1c4720d424cf53f256f7de420e1da
Yago Henrique Fernandes de Almeida <yagof2421@gmail.com>
2025-04-04 20:55:22 -0300

    Feat: Generic repository

---
+++ DB/Model/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace HFA;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+
+    IQueryable<TEntity>? FindAll();
+    IQueryable<TEntity>? FindAll(Expression<Func<TEntity, bool>> predicate);
+    IQueryable<TEntity>? FindAll(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>,IOrderedQueryable<TEntity>>? orderBy = null);
+    TEntity? Find(Expression<Func<TEntity, bool>> predicate);
+    void Add(TEntity entity);
+    void AddMany(IEnumerable<TEntity> entities);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);   
+    void DeleteMany(Expression<Func<TEntity, bool>> predicate);
+    bool Any(Expression<Func<TEntity, bool>> predicate);
+    int Count(Expression<Func<TEntity, bool>> predicate);
+}
---
+++ DB/Model/Repository.cs
@@ -0,0 +1,78 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace HFA.DB.Model
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext context;
+        protected readonly DbSet<TEntity> dbSet;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<TEntity>();
+        }
+        public void Add(TEntity entity)
+        {
+            dbSet.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void AddMany(IEnumerable<TEntity> entities)
+        {
+            dbSet.AddRange(entities);
+            context.SaveChanges();
+        }
+
+        public bool Any(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.Any(predicate);
+        }
+
+        public int Count(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.Count(predicate);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            dbSet.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public void DeleteMany(Expression<Func<TEntity, bool>> predicate)
+        {
+            IQueryable<TEntity> entities = dbSet.Where(predicate);
+            dbSet.RemoveRange(entities);
+            context.SaveChanges();
+        }
+
+        public TEntity? Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.FirstOrDefault(predicate);
+        }
+
+        public IQueryable<TEntity>? FindAll()
+        {
+            return dbSet.AsQueryable();
+        }
+
+        public IQueryable<TEntity>? FindAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            return dbSet.Where(predicate);
+        }
+
+        public IQueryable<TEntity>? FindAll(Expression<Func<TEntity, bool>> predicate, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null)
+        {
+            var query = dbSet.Where(predicate);
+            return orderBy != null ? orderBy(query) : query;
+        }
+
+        public void Update(TEntity entity)
+        {
+            dbSet.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
